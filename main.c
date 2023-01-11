2805c6933099dfcbe6018ecc67f39bfa28984ad1
ProgKea <keanu.daniel@e-mail.de>
2023-01-11 21:45:43 +0100

    cells slower, change the coloring system, highlight current cell

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_mouse.h>
 #include <SDL2/SDL_render.h>
@@ -7,30 +8,94 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <assert.h>
+#include <errno.h>
 
-#define FOREGROUND_COLOR 255, 255, 255, 255
-#define BACKGROUND_COLOR 0, 0, 0, 255
-#define PAUSED_COLOR 50, 50, 50, 255
+#define make_rgba(r, g, b, a) \
+    (rgba)                    \
+    {                         \
+        r, g, b, a            \
+    }
 
-#define CELL_ROWS 20
-#define CELL_COLS 20
-#define CELL_SIZE 50
-#define CELL_COUNT CELL_ROWS *CELL_COLS
+#define unwrap_rgba(rgba) rgba.r, rgba.g, rgba.b, rgba.a
 
-#define DEFAULT_WINDOW_WIDTH CELL_ROWS *CELL_SIZE
-#define DEFAULT_WINDOW_HEIGHT CELL_COLS *CELL_SIZE
+#define FOREGROUND_COLOR make_rgba(225, 225, 225, 255)
+#define BACKGROUND_COLOR make_rgba(0, 0, 0, 255)
+#define HOVER_COLOR make_rgba(100, 100, 100, 255)
+#define PAUSED_COLOR make_rgba(50, 50, 50, 255)
 
-#define RETURN_ERROR(msg)                                 \
+#define CELL_ROWS 30
+#define CELL_COLS 30
+#define CELL_SIZE 35
+#define CELL_COUNT (CELL_ROWS * CELL_COLS)
+#define CELL_UPDATE 20
+
+#define DEFAULT_WINDOW_WIDTH (CELL_ROWS * CELL_SIZE)
+#define DEFAULT_WINDOW_HEIGHT (CELL_COLS * CELL_SIZE)
+
+#define return_error(msg)                                 \
     do {                                                  \
         fprintf(stderr, "%s\n%s\n", msg, SDL_GetError()); \
         return EXIT_FAILURE;                              \
     } while (0)
 
+#define ignore(value) (void)(value + 1)
+
+typedef struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+} rgba;
+
 typedef struct {
     uint32_t row;
     uint32_t col;
     bool dead;
+    rgba fill_color;
+    rgba border_color;
 } Cell;
+
+void zoom(int32_t zoom)
+{
+    ignore(zoom);
+    assert(0 && "unimplemented");
+}
+
+/* void set_grid_str(Cell *, char *s) */
+/* { */
+/*     size_t string_size = strlen(s); */
+/* } */
+
+char *slurp_file(const char *file_path)
+{
+    FILE *file = NULL;
+    char *buffer = NULL;
+
+    {
+        file = fopen(file_path, "rb");
+        if (!file) goto error;
+
+        if (fseek(file, 0, SEEK_END) < 0) goto error;
+
+        long file_size = ftell(file);
+        if (file_size < 0) goto error;
+        buffer = malloc((size_t)file_size + 1);
+        if (!buffer) goto error;
+        if (fseek(file, 0, SEEK_SET) < 0) goto error;
+
+        ignore(fread(buffer, 1, (size_t)file_size, file));
+        if (ferror(file)) goto error;
+        buffer[file_size] = '\0';
+    }
+
+    fclose(file);
+    return buffer;
+
+error:
+    if (file) fclose(file);
+    return NULL;
+}
 
 uint32_t get_index(uint32_t row, uint32_t col)
 {
@@ -44,7 +109,8 @@ uint32_t count_neighbors(Cell *grid, uint32_t row, uint32_t col)
     for (int32_t i = row - 1; i <= (int32_t)row + 1; i++) {
         for (int32_t j = col - 1; j <= (int32_t)col + 1; j++) {
             if (i == (int32_t)row && j == (int32_t)col) continue;
-            if (i >= 0 && i < CELL_ROWS && j >= 0 && j < CELL_COLS && !grid[get_index(i, j)].dead)
+            if (i >= 0 && i < CELL_ROWS && j >= 0 && j < CELL_COLS &&
+                !grid[get_index(i, j)].dead)
                 n++;
         }
     }
@@ -52,15 +118,43 @@ uint32_t count_neighbors(Cell *grid, uint32_t row, uint32_t col)
     return n;
 }
 
-void render_grid(Cell *grid, SDL_Renderer *renderer, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+void render_grid(Cell *grid, SDL_Renderer *renderer)
 {
+    /* for (uint32_t i = 0; i < CELL_COUNT; i++) { */
+    /*     SDL_SetRenderDrawColor(renderer, r, g, b, a); */
+    /*     if (grid[i].dead) { */
+    /*         SDL_RenderDrawRect(renderer, */
+    /*                            &(SDL_Rect){ */
+    /*                                .w = CELL_SIZE, */
+    /*                                .h = CELL_SIZE, */
+    /*                                .x = grid[i].row * CELL_SIZE, */
+    /*                                .y = grid[i].col * CELL_SIZE}); */
+    /*     } else { */
+    /*         SDL_RenderFillRect(renderer, */
+    /*                            &(SDL_Rect){ */
+    /*                                .w = CELL_SIZE, */
+    /*                                .h = CELL_SIZE, */
+    /*                                .x = grid[i].row * CELL_SIZE, */
+    /*                                .y = grid[i].col * CELL_SIZE}); */
+    /*     } */
+    /* } */
+
     for (uint32_t i = 0; i < CELL_COUNT; i++) {
-        SDL_SetRenderDrawColor(renderer, r, g, b, a);
-        if (grid[i].dead) {
-            SDL_RenderDrawRect(renderer, &(SDL_Rect){.w = CELL_SIZE, .h = CELL_SIZE, .x = grid[i].row * CELL_SIZE, .y = grid[i].col * CELL_SIZE});
-        } else {
-            SDL_RenderFillRect(renderer, &(SDL_Rect){.w = CELL_SIZE, .h = CELL_SIZE, .x = grid[i].row * CELL_SIZE, .y = grid[i].col * CELL_SIZE});
-        }
+        SDL_SetRenderDrawColor(renderer, unwrap_rgba(grid[i].fill_color));
+        SDL_RenderFillRect(renderer,
+                           &(SDL_Rect){
+                               .w = CELL_SIZE,
+                               .h = CELL_SIZE,
+                               .x = grid[i].row * CELL_SIZE,
+                               .y = grid[i].col * CELL_SIZE});
+        SDL_SetRenderDrawColor(renderer, unwrap_rgba(grid[i].border_color));
+        SDL_RenderDrawRect(renderer,
+                           &(SDL_Rect){
+                               .w = CELL_SIZE,
+                               .h = CELL_SIZE,
+                               .x = grid[i].row * CELL_SIZE,
+                               .y = grid[i].col * CELL_SIZE});
+        SDL_SetRenderDrawColor(renderer, unwrap_rgba(BACKGROUND_COLOR));
     }
 }
 
@@ -68,20 +162,27 @@ void create_grid(Cell *grid)
 {
     for (uint32_t row = 0; row < CELL_ROWS; row++) {
         for (uint32_t col = 0; col < CELL_COLS; col++) {
-            grid[get_index(row, col)] = (Cell){row, col, true};
+            grid[get_index(row, col)] = (Cell){row, col, true, BACKGROUND_COLOR, FOREGROUND_COLOR};
         }
     }
 }
 
+void swap_cells(Cell *a, Cell *b)
+{
+    Cell *t = a;
+    a->dead = b->dead;
+    b->dead = t->dead;
+}
+
 void update_grid(Cell *grid)
 {
-    static Cell grid_backup[CELL_COUNT] = {0};
-    memcpy(grid_backup, grid, CELL_COUNT * sizeof(Cell));
+    static Cell old_grid[CELL_COUNT] = {0};
+    memcpy(old_grid, grid, CELL_COUNT * sizeof(Cell));
 
     for (int32_t row = 0; row < CELL_ROWS; row++) {
         for (int32_t col = 0; col < CELL_COLS; col++) {
             Cell *current_cell = &grid[get_index(row, col)];
-            uint32_t neighbors = count_neighbors(grid_backup, row, col);
+            uint32_t neighbors = count_neighbors(old_grid, row, col);
 
             if (current_cell->dead) {
                 if (neighbors == 3) {
@@ -101,7 +202,7 @@ static Cell grid[CELL_COUNT];
 int32_t main(void)
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        RETURN_ERROR("SDL_Init failed.");
+        return_error("SDL_Init failed.");
     }
 
     SDL_Window *win = SDL_CreateWindow("Game of life",
@@ -109,14 +210,14 @@ int32_t main(void)
                                        DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT,
                                        SDL_WINDOW_SHOWN);
     if (!win) {
-        RETURN_ERROR("Window creation failed.");
+        return_error("Window creation failed.");
     }
 
     SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (!renderer) {
         SDL_DestroyRenderer(renderer);
         SDL_Quit();
-        RETURN_ERROR("Renderer creation failed.");
+        return_error("Renderer creation failed.");
     }
 
     create_grid(grid);
@@ -124,18 +225,26 @@ int32_t main(void)
     bool paused = true;
     SDL_Event e;
     int32_t mousex, mousey;
+    int32_t mouse_cell_idx = 0;
+    int32_t update_frames = 0;
+
+    char *test = slurp_file("./test.txt");
+    printf("%s\n", test);
+
     while (!quit) {
         SDL_PollEvent(&e);
         switch (e.type) {
         case SDL_QUIT:
             quit = true;
             break;
-        case SDL_MOUSEBUTTONDOWN:
+        case SDL_MOUSEMOTION:
             SDL_GetMouseState(&mousex, &mousey);
-            int32_t row = mousex / CELL_SIZE % CELL_ROWS;
-            int32_t col = mousey / CELL_SIZE % CELL_COLS;
-            int32_t idx = get_index(row, col);
-            if (idx >= 0) grid[idx].dead ^= true;
+            int32_t mouse_row = mousex / CELL_SIZE % CELL_ROWS;
+            int32_t mouse_col = mousey / CELL_SIZE % CELL_COLS;
+            mouse_cell_idx = get_index(mouse_row, mouse_col);
+            break;
+        case SDL_MOUSEBUTTONDOWN:
+            grid[mouse_cell_idx].dead ^= true;
             break;
         case SDL_KEYDOWN:
             switch (e.key.keysym.sym) {
@@ -152,17 +261,34 @@ int32_t main(void)
             break;
         }
 
-        SDL_SetRenderDrawColor(renderer, BACKGROUND_COLOR);
         SDL_RenderClear(renderer);
 
+        // FIXME: This code is not really that clean
+        for (uint32_t i = 0; i < CELL_COUNT; i++) {
+            if (!grid[i].dead)
+                grid[i].fill_color = FOREGROUND_COLOR;
+            else
+                grid[i].fill_color = BACKGROUND_COLOR;
+
+            if (paused)
+                grid[i].border_color = PAUSED_COLOR;
+            else
+                grid[i].border_color = FOREGROUND_COLOR;
+        }
+        if (grid[mouse_cell_idx].dead) grid[mouse_cell_idx].fill_color = HOVER_COLOR;
+
         if (!paused) {
-            update_grid(grid);
-            render_grid(grid, renderer, FOREGROUND_COLOR);
+            if (update_frames >= CELL_UPDATE) {
+                update_grid(grid);
+                update_frames = 0;
+            }
+            render_grid(grid, renderer);
         } else {
-            render_grid(grid, renderer, PAUSED_COLOR);
+            render_grid(grid, renderer);
         }
 
         SDL_RenderPresent(renderer);
+        update_frames++;
     }
 
     SDL_DestroyRenderer(renderer);
@@ -171,7 +297,6 @@ int32_t main(void)
     return EXIT_SUCCESS;
 }
 
-// TODO: make it wrap
 // TODO: read file to generate the grid or let the user click cells in order to toggle them
 // TODO: add zoom function
-// TODO: make it slower
+// TODO: make it wrap
