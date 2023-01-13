2c478d29058cc45ffb3f1d8b5274d6889ac53659
ProgKea <keanu.daniel@e-mail.de>
2023-01-14 00:11:30 +0100

    Add loading and saving of the grid

--- main.c
+++ main.c
@@ -1,8 +1,4 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_keycode.h>
-#include <SDL2/SDL_mouse.h>
-#include <SDL2/SDL_render.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -56,16 +52,76 @@ typedef struct {
     rgba border_color;
 } Cell;
 
-void zoom(int32_t zoom)
+void zoom(int zoom)
 {
     ignore(zoom);
     assert(0 && "unimplemented");
 }
 
-/* void set_grid_str(Cell *, char *s) */
-/* { */
-/*     size_t string_size = strlen(s); */
-/* } */
+uint32_t get_index(uint32_t row, uint32_t col)
+{
+    return row + col * CELL_COLS;
+}
+
+void remove_newlines(char *s)
+{
+    size_t i, di = 0;
+    for (i = 0; i < strlen(s); i++) {
+        if (s[i] == '\n') {
+            di++;
+            continue;
+        }
+        s[i - di] = s[i];
+    }
+    s[i - di + 1] = '\0';
+}
+
+void set_grid_str(Cell *grid, char *s)
+{
+    remove_newlines(s);
+
+    uint32_t di = 0;
+
+    for (uint32_t i = 0; i < CELL_COUNT; i++) {
+        if (s[i] == '\n') {
+            di++;
+            continue;
+        }
+        grid[i + di].dead = s[i + di] == '0';
+    }
+}
+
+typedef int Errno;
+
+#define return_defer(value) \
+    do {                    \
+        result = (value);   \
+        goto defer;         \
+    } while (0);
+
+Errno save_grid_to_file(Cell *grid, const char *file_path)
+{
+    int result = 0;
+    FILE *file = NULL;
+
+    {
+        file = fopen(file_path, "wb");
+        if (!file) return_defer(errno);
+
+        for (uint32_t col = 0; col < CELL_COLS; col++) {
+            for (uint32_t row = 0; row < CELL_ROWS; row++) {
+                fprintf(file, "%d", !grid[get_index(row, col)].dead);
+                if (ferror(file)) return_defer(errno);
+            }
+            fprintf(file, "\n");
+            if (ferror(file)) return_defer(errno);
+        }
+    }
+
+defer:
+    if (file) fclose(file);
+    return result;
+}
 
 char *slurp_file(const char *file_path)
 {
@@ -97,18 +153,13 @@ error:
     return NULL;
 }
 
-uint32_t get_index(uint32_t row, uint32_t col)
-{
-    return row + col * CELL_COLS;
-}
-
 uint32_t count_neighbors(Cell *grid, uint32_t row, uint32_t col)
 {
     uint32_t n = 0;
 
-    for (int32_t i = row - 1; i <= (int32_t)row + 1; i++) {
-        for (int32_t j = col - 1; j <= (int32_t)col + 1; j++) {
-            if (i == (int32_t)row && j == (int32_t)col) continue;
+    for (int i = row - 1; i <= (int)row + 1; i++) {
+        for (int j = col - 1; j <= (int)col + 1; j++) {
+            if (i == (int)row && j == (int)col) continue;
             if (i >= 0 && i < CELL_ROWS && j >= 0 && j < CELL_COLS &&
                 !grid[get_index(i, j)].dead)
                 n++;
@@ -120,25 +171,6 @@ uint32_t count_neighbors(Cell *grid, uint32_t row, uint32_t col)
 
 void render_grid(Cell *grid, SDL_Renderer *renderer)
 {
-    /* for (uint32_t i = 0; i < CELL_COUNT; i++) { */
-    /*     SDL_SetRenderDrawColor(renderer, r, g, b, a); */
-    /*     if (grid[i].dead) { */
-    /*         SDL_RenderDrawRect(renderer, */
-    /*                            &(SDL_Rect){ */
-    /*                                .w = CELL_SIZE, */
-    /*                                .h = CELL_SIZE, */
-    /*                                .x = grid[i].row * CELL_SIZE, */
-    /*                                .y = grid[i].col * CELL_SIZE}); */
-    /*     } else { */
-    /*         SDL_RenderFillRect(renderer, */
-    /*                            &(SDL_Rect){ */
-    /*                                .w = CELL_SIZE, */
-    /*                                .h = CELL_SIZE, */
-    /*                                .x = grid[i].row * CELL_SIZE, */
-    /*                                .y = grid[i].col * CELL_SIZE}); */
-    /*     } */
-    /* } */
-
     for (uint32_t i = 0; i < CELL_COUNT; i++) {
         SDL_SetRenderDrawColor(renderer, unwrap_rgba(grid[i].fill_color));
         SDL_RenderFillRect(renderer,
@@ -179,8 +211,8 @@ void update_grid(Cell *grid)
     static Cell old_grid[CELL_COUNT] = {0};
     memcpy(old_grid, grid, CELL_COUNT * sizeof(Cell));
 
-    for (int32_t row = 0; row < CELL_ROWS; row++) {
-        for (int32_t col = 0; col < CELL_COLS; col++) {
+    for (int row = 0; row < CELL_ROWS; row++) {
+        for (int col = 0; col < CELL_COLS; col++) {
             Cell *current_cell = &grid[get_index(row, col)];
             uint32_t neighbors = count_neighbors(old_grid, row, col);
 
@@ -199,7 +231,7 @@ void update_grid(Cell *grid)
 
 static Cell grid[CELL_COUNT];
 
-int32_t main(void)
+int main(void)
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         return_error("SDL_Init failed.");
@@ -224,12 +256,12 @@ int32_t main(void)
     bool quit = false;
     bool paused = true;
     SDL_Event e;
-    int32_t mousex, mousey;
-    int32_t mouse_cell_idx = 0;
-    int32_t update_frames = 0;
+    int mousex, mousey;
+    int mouse_cell_idx = 0;
+    int update_frames = 0;
 
-    char *test = slurp_file("./test.txt");
-    printf("%s\n", test);
+    char *txt_grid = slurp_file("gol.txt");
+    set_grid_str(grid, txt_grid);
 
     while (!quit) {
         SDL_PollEvent(&e);
@@ -239,8 +271,8 @@ int32_t main(void)
             break;
         case SDL_MOUSEMOTION:
             SDL_GetMouseState(&mousex, &mousey);
-            int32_t mouse_row = mousex / CELL_SIZE % CELL_ROWS;
-            int32_t mouse_col = mousey / CELL_SIZE % CELL_COLS;
+            int mouse_row = mousex / CELL_SIZE % CELL_ROWS;
+            int mouse_col = mousey / CELL_SIZE % CELL_COLS;
             mouse_cell_idx = get_index(mouse_row, mouse_col);
             break;
         case SDL_MOUSEBUTTONDOWN:
@@ -256,6 +288,9 @@ int32_t main(void)
                 break;
             case SDLK_r:
                 create_grid(grid);
+                break;
+            case SDLK_s:
+                save_grid_to_file(grid, "gol.txt");
                 break;
             }
             break;
@@ -297,6 +332,9 @@ int32_t main(void)
     return EXIT_SUCCESS;
 }
 
-// TODO: read file to generate the grid or let the user click cells in order to toggle them
 // TODO: add zoom function
 // TODO: make it wrap
+// TODO: fix the rows and cols being mixed up in the code
+// TODO: find a way to make CELL_ROWS and CELL_COLS variable
+// TODO: Set a maximum amount of cells and let the user decide the cells
+// TODO: read the file path from command line arguments and load the cells of the provided file
